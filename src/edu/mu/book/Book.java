01640df4962d55faff6174dcb7d33bd2aa0471b8
Tanner Pack <tannerpack@Tanners-MacBook-Air.local>
2025-02-12 15:11:19 -0600

    Created Book class. Added the private attributes as well as the constructors and getter methods.

--- src/edu/mu/book/Book.java
+++ src/edu/mu/book/Book.java
@@ -3,8 +3,35 @@ package edu.mu.book;
 public class Book {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+// Private attributes
+		private String title;
+		private String author;
+		private String IBSN;
+		private double price;
+
+//Default constructor	
+public Book() {
+	this.title = "Unknown";
+	this.author = "Unkown";
+	this.ISBN = "Unknown";
+	this.price = 0.0;
+}
+
+//Parameterized constructor
+public Book(String title, String author, String IBSN, double price) {
+	this.title = title;
+	this.author = author;
+	this.IBSN = IBSN;
+	this.price = price;
+}
+
+//Copy constructor
+public Book(Book other) {
+	this.title = other.title;
+	this.author = other.author;
+	this.ISBN = other.ISBN;
+	this.price = other.price;
+}
 
 	}
 
