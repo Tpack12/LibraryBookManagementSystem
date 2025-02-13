96e678bfd9575aee0796d96343d971f8fb132fc0
Jacob Stirnemann <jacobstirnemann@Jacobs-MacBook-Pro.local>
2025-02-12 22:18:59 -0600

    Added three books to the library.

--- src/edu/mu/book/LibraryApp.java
+++ src/edu/mu/book/LibraryApp.java
@@ -3,8 +3,16 @@ package edu.mu.book;
 public class LibraryApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Library library = new Library();
+		
+		//Adding books to the library
+		Book book1 = new Book("9780547928227", "Hobbit", "J.R.R. Tolkien");
+		Book book2 = new Book("9781338865394", "Captain Underpants", "Dav Pilkey");
+		Book book3 = new Book("9780452262935", "1984", "George Orwell");
+		
+		library.addBook(book1);
+		library.addBook(book2);
+		library.addBook(book3);
 	}
 
 }
